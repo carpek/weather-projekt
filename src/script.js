546daf36ec56da3e92bc1f92391fcff2dd8f6964
carpek <77640624+carpek@users.noreply.github.com>
2021-03-21 14:43:15 +0100

    update js code fixed bug

--- src/script.js
+++ src/script.js
@@ -33,93 +33,83 @@ function dayFormat(dates) {
   }
 
   
-// display forcast of hourly weather
-
-function displayForecast (response) {
-
-    let  forecastElement = document.querySelector("#forcast");
-    forecastElement.innerHTML = null;
-    let forecast = null;
-    
-    for (let index = 0; index < 6; index++) {
-    forecast = response.data.list[index];
-    forecastElement.innerHTML += `
-
-    <div class="col">
-        <div class="cardSmall">
-            <div class="card-body">
-            ${formatHours(forecast.dt*1000)}
-            
-            <img
-                src="http://openweathermap.org/img/wn/${
-                forecast.weather[0].icon}@2x.png"
-                />
-
-            <span class="forcast-temperature">
-                 <strong>${Math.round(forecast.main.temp_max)}°
-                 </strong>
-                 
-                 ${Math.round(forecast.main.temp_min)}
-            
-                 </span>
-            </div>        
-        </div>
-    </div> `;}
-
-    }
-
-// Search city & handle submit
-
-  function search(city) {
-    let apiKey = `a8b18d5e574b14c1d3de44331ec7e970`;
-    let apiEndpoint = "https://api.openweathermap.org/data/2.5/weather";
-    let units = "metric";
-    let apiUrl = `${apiEndpoint}?q=${city}&appid=${apiKey}&units=${units}`;
-    
-    axios.get(apiUrl).then(showTemperature);
-
-    apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=metric`;
-    axios.get(apiUrl).then(displayForecast);
-
-  }
-  
-  function handleSubmit(event) {
-    event.preventDefault();
-    let cityElementSearch = document.querySelector("#search-input");
-    search(cityElementSearch.value);
-  }
-  
   // display Elements in html 
 
  function showTemperature(response) {
     let temperatureElement = document.querySelector("#temperature");
-    temperatureElement.innerHTML = Math.round(celciusTemperatureDisplay);
+    let cityElement = document.querySelector("#current-city");
+    let descriptionElement = document.querySelector("#description");
+    let humidityElement = document.querySelector("#current-humidity");
+    let windElement = document.querySelector("#current-wind");
+    let currentDate = document.querySelector("#date");
 
     celciusTemperatureDisplay = response.data.main.temp;
 
-let cityElement = document.querySelector("#current-city");
-cityElement.innerHTML = response.data.name;
-
-let descriptionElement = document.querySelector("#description");
-descriptionElement.innerHTML = response.data.weather[0].description;
-
-let humidityElement = document.querySelector("#current-humidity");
-humidityElement.innerHTML = response.data.main.humidity;
-
-let windElement = document.querySelector("#current-wind");
-windElement.innerHTML = Math.round(response.data.wind.speed);
-
-let currentDate = document.querySelector("#date");
-currentDate.innerHTML = dayFormat(response.data.dt*1000);
-
-
+    temperatureElement.innerHTML = Math.round(celciusTemperatureDisplay);    
+    cityElement.innerHTML = response.data.name;
+    descriptionElement.innerHTML = response.data.weather[0].description;
+    humidityElement.innerHTML = response.data.main.humidity;
+    windElement.innerHTML = Math.round(response.data.wind.speed);
+    currentDate.innerHTML = dayFormat(response.data.dt*1000);
 
 }
 
-let searchForm = document.querySelector("#search-form");
-searchForm.addEventListener("submit", handleSubmit);
- 
+// display forcast of hourly weather
 
+function displayForecast (response) {
+
+  let  forecastElement = document.querySelector("#forcast");
+  forecastElement.innerHTML = null;
+  let forecast = null;
+  
+  for (let index = 0; index < 6; index++) {
+  forecast = response.data.list[index];
+  forecastElement.innerHTML += `
+
+  <div class="col">
+      <div class="cardSmall">
+          <div class="card-body">
+          ${formatHours(forecast.dt*1000)}
+          
+          <img
+              src="http://openweathermap.org/img/wn/${
+              forecast.weather[0].icon}@2x.png"
+              />
+
+          <span class="forcast-temperature">
+               <strong>${Math.round(forecast.main.temp_max)}°
+               </strong>
+               
+               ${Math.round(forecast.main.temp_min)}
+          
+               </span>
+          </div>        
+      </div>
+  </div> `;}
+
+  }
+
+
+// Search city & handle submit
+
+function search(city) {
+  let apiKey = `a8b18d5e574b14c1d3de44331ec7e970`;
+  let apiEndpoint = "https://api.openweathermap.org/data/2.5/weather";
+  let units = "metric";
+  let apiUrl = `${apiEndpoint}?q=${city}&appid=${apiKey}&units=${units}`;
+  
+  axios.get(apiUrl).then(showTemperature);
+
+  apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
+
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityElementSearch = document.querySelector("#search-input");
+  search(cityElementSearch.value);
+}
 
  // display celcius & fahrenheit on click 
 
@@ -127,11 +117,11 @@ searchForm.addEventListener("submit", handleSubmit);
   function fahrenheitClick(event) {
     event.preventDefault();
     let temperatureElement = document.querySelector("#temperature");
+    
     celciusTemperature.classList.remove("units");
     fahrenheitTemperature.classList.add("units");
 
     let fahrenheit =  (celciusTemperatureDisplay * 9) / 5 + 32;
-   
     temperatureElement.innerHTML = Math.round(fahrenheit);
   }
   
@@ -146,6 +136,9 @@ searchForm.addEventListener("submit", handleSubmit);
   }
 
   let celciusTemperatureDisplay = null;
+
+  let searchForm = document.querySelector("#search-form");
+  searchForm.addEventListener("submit", handleSubmit);
   
   let fahrenheitTemperature = document.querySelector("#fahrenheit-temperature");
   fahrenheitTemperature.addEventListener("click", fahrenheitClick);
